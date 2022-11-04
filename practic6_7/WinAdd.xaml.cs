e996a779a103ce620eff5bf3a5a76dd67d7f869f
Danil <historiezz1@gmail.com>
2022-11-05 02:25:30 +0300

    add errors detect

--- practic6_7/WinAdd.xaml.cs
+++ practic6_7/WinAdd.xaml.cs
@@ -27,15 +27,29 @@ namespace practic6_7
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
-            try 
-            { 
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrEmpty(tbKind.Text))
+                errors.AppendLine("Укажите вид работы");
+            if (string.IsNullOrEmpty(dpDateCreate.Text))
+                errors.AppendLine("Укажите дату создания");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            else
+            {
+                try
+                { 
                 _db.GetContext().work.Add(new work { kind_material = tbKind.Text, date_create = dpDateCreate.SelectedDate.Value });
                 _db.GetContext().SaveChanges();
                 MessageBox.Show("Успешно!");
                 new WinAdd_Del().Show();
                 Hide();
             }
-            catch { MessageBox.Show("Ошибка!"); }
+            catch { MessageBox.Show("Неверный тип данных!"); }
         }
     }
 }
